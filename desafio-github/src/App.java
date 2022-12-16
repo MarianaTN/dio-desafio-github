e847f5285a70fca19278b599e405727e3cc2d325
Mariana Trevizani <mariana.trevizani@yahoo.com.br>
2022-12-16 00:09:45 -0300

    alteração de mensagens no app

--- desafio-github/src/App.java
+++ desafio-github/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Olá, bem vindo ao repositório do desafio de GitHub.");
+        System.out.println("Foi criado para conclusão do desafio referente ao bootcamp.");
+        System.out.println("Nos vemos no próximo desafio.");
     }
 }
